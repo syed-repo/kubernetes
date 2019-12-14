a245b8566dfc945f0666ba67d561bbc55f1f328c
Ubuntu <ubuntu@ip-172-31-4-214.us-east-2.compute.internal>
2019-12-14 17:48:22 +0000

    included metrics server

--- kubernetes.cs
+++ kubernetes.cs
@@ -2,3 +2,5 @@ ubuntu@ip-172-31-4-214:~/kubernetes/flask$ kubectl port-forward flask-ffb4d5bf4-
 Forwarding from 127.0.0.1:5000 -> 5000
 Forwarding from [::1]:5000 -> 5000
 Handling connection for 5000
+
+kubectl delete -n kube-system deployments.apps metrics-server
