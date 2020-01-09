546d5ee99277ce2b7d3f90c309846a7d468d3027
Ubuntu <ubuntu@ip-172-31-34-214.us-east-2.compute.internal>
2020-01-09 18:20:50 +0000

    Notes

--- java.cs
+++ java.cs
@@ -1,10 +1,12 @@
-Installing java version 8
+Install Java on Ubuntu 18.04
+----------------------------
+sudo apt update
+sudo apt install openjdk-8-jdk
+./usr/lib/jvm/java-8-openjdk-amd64/
 
-ubuntu@ip-172-31-4-214:~$ sudo apt-get install openjdk-8-jdk
-
-ubuntu@ip-172-31-4-214:~$ java -version
+ubuntu@ip-172-31-34-214:/usr/lib/jvm/java-8-openjdk-amd64/bin$ java -version
 openjdk version "1.8.0_232"
 OpenJDK Runtime Environment (build 1.8.0_232-8u232-b09-0ubuntu1~18.04.1-b09)
 OpenJDK 64-Bit Server VM (build 25.232-b09, mixed mode)
-ubuntu@ip-172-31-4-214:~$
+ubuntu@ip-172-31-34-214:/usr/lib/jvm/java-8-openjdk-amd64/bin$
 
