8c467e73630b1551a375fa8a18bd8af17d4f4ede
Ubuntu <ubuntu@ip-172-31-4-214.us-east-2.compute.internal>
2020-03-13 12:31:47 +0000

    metrics script

---
+++ measure.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+counter=1
+echo "Metrics Server Information" > /home/ubuntu/kubernetes/metrics.dat
+while [ $counter -lt 1000 ]
+do
+    op=`kubectl top pod | grep vflow`
+    dt=`date`
+    echo "$dt $op" >> /home/ubuntu/kubernetes/metrics.dat
+    echo "$dt $op"
+    sleep 0.5
+done
