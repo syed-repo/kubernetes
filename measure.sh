90ff8a73681df0c4575f7f5e6c46b824ccf14cbe
Ubuntu <ubuntu@ip-172-31-4-214.us-east-2.compute.internal>
2020-03-13 17:21:11 +0000

    updates to collector testing

--- measure.sh
+++ measure.sh
@@ -3,7 +3,7 @@ counter=1
 echo "Metrics Server Information" > /home/ubuntu/kubernetes/metrics.dat
 while [ $counter -lt 1000 ]
 do
-    op=`kubectl top pod | grep vflow`
+    op=`kubectl top pod | grep netflow`
     dt=`date`
     echo "$dt $op" >> /home/ubuntu/kubernetes/metrics.dat
     echo "$dt $op"
