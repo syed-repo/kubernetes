a5af4cd5a84e286966e924b2e4397cc012ebe6b9
Ubuntu <ubuntu@ip-172-31-4-214.us-east-2.compute.internal>
2020-03-25 04:12:15 +0000

    collector cleanup

--- netflow/measure.sh
+++ netflow/measure.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 counter=1
-echo "Metrics Server Information" > /home/ubuntu/kubernetes/metrics.dat
+echo "Metrics Server Information" > ./metrics.dat
 while [ $counter -lt 1000 ]
 do
     op=`kubectl top pod | grep jcoll`
     dt=`date`
-    echo "$dt $op" >> /home/ubuntu/kubernetes/metrics.dat
+    echo "$dt $op" >> ./metrics.dat
     echo "$dt $op"
     sleep 0.5
 done
+echo "Script completed"
